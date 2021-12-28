df7a9f0dc477b005c49e991b322a38af5e2c4389
hecuhecu <hecunyuji5@gmail.com>
2021-12-28 13:56:29 +0900

    first commit

--- hirokiKadai5/ContentView.swift
+++ hirokiKadai5/ContentView.swift
@@ -8,9 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var num1: Double?
+    @State private var num2: Double?
+    @State private var total: Double = 0
+    @State private var errorMessage: String?
+    @State private var isError: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            HStack {
+                NumberInputView(num: $num1)
+                NumberInputView(num: $num2)
+            }
+            
+            Button(action: {
+                divide()
+            }, label: { Text("計算") })
             .padding()
+            .alert(isPresented: $isError) {
+                Alert(title: Text("課題5"), message: Text(errorMessage!), dismissButton: .default(Text("OK")))
+            }
+            
+            Text("\(total)")
+        }
+        
+    }
+    
+    func divide() {
+        guard let _num1 = num1 else {
+            isError = true
+            errorMessage = "割る数を入力して下さい"
+            return
+        }
+        
+        guard let _num2 = num2 else {
+            isError = true
+            errorMessage = "割られる数を入力して下さい"
+            return
+        }
+        
+        if _num2 == 0 {
+            isError = true
+            errorMessage = "割る数には0を入力しないで下さい"
+            return
+        }
+        
+        isError = false
+        total = _num1 / _num2
+    }
+    
+}
+
+struct NumberInputView: View {
+    @Binding var num: Double?
+    
+    var body: some View {
+        TextField("", value: $num, formatter: NumberFormatter())
+            .padding(.horizontal)
+            .keyboardType(.numberPad)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
     }
 }
 
